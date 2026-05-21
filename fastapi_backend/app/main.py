8cf660c2b256e9c945565287afcbd586f05628e8
G-anrabuS <subarna.ghosh0123@gmail.com>
2026-05-21 08:31:04 +0530

    CORS fixed

--- fastapi_backend/app/main.py
+++ fastapi_backend/app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # <-- ADD THIS IMPORT
 from starlette.middleware.sessions import SessionMiddleware
 from app.api import auth, repos, scan
 from app.core.config import JWT_SECRET
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:8080",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_middleware(SessionMiddleware, secret_key=JWT_SECRET, same_site="lax")
 
