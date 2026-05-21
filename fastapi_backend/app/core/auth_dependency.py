271c49d87e7633ff5040c27f37c6862279329562
G-anrabuS <subarna.ghosh0123@gmail.com>
2026-05-21 18:51:04 +0530

    Auth dependency updated

--- fastapi_backend/app/core/auth_dependency.py
+++ fastapi_backend/app/core/auth_dependency.py
@@ -1,16 +1,15 @@
-from fastapi import Header, HTTPException
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose import jwt, JWTError
 from app.core.config import JWT_SECRET
 
 ALGORITHM = "HS256"
 
+security = HTTPBearer()
 
-def get_current_user(authorization: str = Header(None)):
-    print("AUTH HEADER RECEIVED:", authorization)
-    if not authorization:
-        raise HTTPException(status_code=401, detail="Missing token")
 
-    token = authorization.replace("Bearer ", "")
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
 
     try:
         payload = jwt.decode(token, JWT_SECRET, algorithms=[ALGORITHM])
